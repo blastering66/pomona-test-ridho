556405082f18ebd6dada1312f7c00ef462015ebd
ridhoCheers <ridho@yourcheersapp.com>
2019-08-06 01:49:13 +0700

    UI: Home Done

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,27 +1,110 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { View, Text, Image, Platform, StyleSheet, TouchableOpacity} from 'react-native'
+import { View, Text, Image, Platform, StyleSheet, TouchableOpacity, ActivityIndicator, SectionList } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import { signout } from '../../store/actions/authActions'
 import { Button } from '../../components'
 import { connect } from 'react-redux'
 import { COLORS } from '../../utils/res'
+import { Satellite } from '../../utils'
+import { ENDPOINT } from '../../utils/config'
 import styled from './styled'
 
 type Props = {
   dispatch: dispatch
 }
-
 @connect((store)=>{
   return {
   }
 })
+
 class Home extends Component<Props> {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: [],
+      runned: false,
+      isLoading: true,
+      refreshing: true
+    }
+  }
+
   componentDidMount() {
+    if (!this.state.runned) {
+      this.getMyList()
+    }
+  }
+
+  getMyList() {
+    this.setState({ runned: true }, () => {
+      Satellite.get(ENDPOINT.ALL_TODO)
+        .then((response) => {
+          this.setState({ isLoading: false, refreshing: false, data: response.data.data })
+          console.log('getMyList', response)
+        }).catch((err) => {
+          const message = err.response.data.data.message
+          console.log('ERROR', message)
+          Alert.alert('Attention', message)
+          this.setState({ isLoading: false, refreshing: false })
+        })
+    })
+
+  }
+
+  doRefresh() {
+    this.setState({
+      refreshing: true
+    }, () => this.getMyList())
+  }
+
+  goToAddPage() {
+    this.props.navigation.navigate('AddNew')
+  }
+
+  goToDetail() {
+    this.props.navigation.navigate('Detail')
   }
 
   render() {
+    const { data, isLoading, refreshing } = this.state
     return (
-      <View style={[ styled.container, { justifyContent: 'center', alignItems: 'center' } ]}>
+      <View style={[ styled.container, { justifyContent: 'center', alignItems: 'center', paddingTop: 20 } ]}>
+        { isLoading ? (
+          <View style={{ alignSelf: 'center' }}>
+            <ActivityIndicator size={'large'} color={COLORS.colorAppTheme} />
+          </View>
+        ) : (
+          <SectionList
+            ref='SectionList'
+            style={{ width: '100%', heigth: '100%', paddingTop: 50, paddingHorizontal: 10 }}
+            sections={[ { title: 'My Todo', data: data } ]}
+            stickySectionHeadersEnabled={false}
+            renderSectionHeader={({ section: { title } }) => {
+              return (
+                <Text style={[ styled.title, { padding: 20, letterSpacing: 0 }]}>{title}</Text>
+              )
+            }}
+            refreshing={refreshing}
+            onRefresh={() => {
+              this.doRefresh()
+            }}
+            renderItem={({ item, index, section }) => {
+              return (
+                <View style={styled.container_row}>
+                  <Text style={styled.row_title}>{item.title}</Text>
+                  <Text style={styled.row_note}>{item.note}</Text>
+                  <View style={{ alignSelf: 'center', position: 'absolute', top: -20, right: 10, flexDirection: 'row' }}>
+                    <Ionicons name='md-attach' size={35} color={'#d4d8d9'} style={{ margin: 8 }} />
+                    <Ionicons name='md-attach' size={35} color={'#d4d8d9'} style={{ margin: 8 }}/>
+                  </View>
+                </View>
+              )
+            }}
+
+          />
+        )}
+
+        <Button style={{ flexDirection: 'column', position: 'absolute',  top: 40, right: 20 }} onPress={() => this.goToAddPage()} buttonText={'New Todo'} backgroundColor={'transparent'} buttonTextColor={COLORS.text} />
       </View>
     )
   }
--- src/screens/Home/styled.js
+++ src/screens/Home/styled.js
@@ -18,6 +18,25 @@ export default StyleSheet.create({
     fontWeight: '500',
     letterSpacing: 2
   },
+  container_row: {
+    flexDirection: 'column',
+    backgroundColor: COLORS.colorWhite,
+    padding: 10
+  },
+  row_title: {
+    fontSize: 18,
+    color: '#000',
+    fontWeight: '500',
+    letterSpacing: 2,
+    padding: 5
+  },
+  row_note: {
+    fontSize: 16,
+    color: '#000',
+    fontWeight: '100',
+    letterSpacing: 1,
+    padding: 5
+  },
   input: {
     paddingTop: 20,
     paddingBottom: 20,
